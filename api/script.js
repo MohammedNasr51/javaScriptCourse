3a698cb9e73b98db239391e1bb5a9804aae70212
Mohamed Nasr <mnasr0868@gmail.com>
2026-04-28 18:09:01 +0300

    api fetching

---
+++ api/script.js
@@ -0,0 +1,52 @@
+//fetch api: fetch(url,{method:method,body:body,headers:headers})
+
+//~using then catch
+// fetch("https://fakestoreapi.com/products", {
+//   method: "GET",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// })
+//   .then((response) => {
+//     if (!response.ok) throw new Error("Network response was not ok");
+//     return response.json();
+//   })
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
+
+//~using async await
+const cardContainer = document.querySelector(".card-container");
+
+// const fetchData = async () => {
+//   try {
+//     cardContainer.innerHTML = "Loading...";
+//     const response = await fetch("https://fakestoreapi.com/products");
+//     console.log(response);
+//     if (!response.ok) throw new Error(`Response status: ${response.status}`);
+//     const data = await response.json();
+//     console.log(data);
+//     if (data.length > 0) cardContainer.innerHTML = "data loaded successfully";
+//     cardContainer.innerHTML = "no data found";
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// };
+// fetchData();
+// const fetchData = async () => {
+//   try {
+//     cardContainer.innerHTML = "Loading...";
+//     const response = await fetch("data.json");
+//     console.log(response);
+//     if (!response.ok) throw new Error(`Response status: ${response.status}`);
+//     const data = await response.json();
+//     console.log(data);
+//     if (data.length > 0) {
+//       cardContainer.innerHTML = "data loaded successfully";
+//     } else {
+//       cardContainer.innerHTML = "no data found";
+//     }
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// };
+// fetchData();
